89472ae1262e05fd589f4241bdeedef3d96198cd
luisafischerv <lfvm@cesar.school>
2026-09-02 14:24:18 -0300

    fix: corrige condicao de corrida no openmp movendo variaveis para dentro do loop paralelo

--- lfvm/mandelbrot_openmp.c
+++ lfvm/mandelbrot_openmp.c
@@ -4,16 +4,13 @@
 
 void openmp(int **matriz, int largura, int altura, int max_interacoes, int num_threads) {
     omp_set_num_threads(num_threads);
-    double c_real;
-    double c_imag;
-    int interacoes;
-
+    
     #pragma omp parallel for
     for (int linha = 0; linha < altura; linha++) {
         for (int coluna = 0; coluna < largura; coluna++) {
-            c_real = pixel_para_real(coluna, largura);
-            c_imag = pixel_para_imag(linha, altura);
-            interacoes = mandelbrot_ponto(c_real, c_imag, max_interacoes);
+            double c_real = pixel_para_real(coluna, largura);
+            double c_imag = c_imag = pixel_para_imag(linha, altura);
+            int interacoes = mandelbrot_ponto(c_real, c_imag, max_interacoes);
             matriz[linha][coluna] = normalizar_intensidade(interacoes, max_interacoes);
         }
     }
